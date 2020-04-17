3fc2b5ead8b1ef1997debd6acddf5b62af8e0eeb
Nicolai Weitkemper <nico.weio@gmail.com>
2020-04-17 11:32:09 +0200

    initial commit (package generator)

---
+++ spec/aw-watcher-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AwWatcherAtomView from '../lib/aw-watcher-atom-view';
+
+describe('AwWatcherAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
