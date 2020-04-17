823cb07234ab01a9f942e89bd8d4e5cf8ece04b4
Nicolai Weitkemper <nico.weio@gmail.com>
2020-04-17 20:38:58 +0200

    remove unnecessary files

--- spec/aw-watcher-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AwWatcherAtomView from '../lib/aw-watcher-atom-view';
-
-describe('AwWatcherAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
